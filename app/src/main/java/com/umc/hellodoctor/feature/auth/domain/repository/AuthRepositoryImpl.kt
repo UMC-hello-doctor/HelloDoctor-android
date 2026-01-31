4306e8f0206d5ec9e206c92d32494c549f4164c5
sfpahs <119095356+sfpahs@users.noreply.github.com>
2026-01-31 22:34:51 +0900

    fix : api 재세팅
    base api respone 설정 및 적용
    access, refresh, googleid 관련 매니저 세팅,
    로그인 시 로그인 이전 토큰 삭제후 로그인 로직 수행
    토큰 저장시 액세스토큰은 인터럽터에 자동으로 담기게 설정(그냥 호출만 하면됨)

--- app/src/main/java/com/umc/hellodoctor/feature/auth/domain/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/umc/hellodoctor/feature/auth/domain/repository/AuthRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.umc.hellodoctor.feature.auth.domain.repository
 
+import com.umc.hellodoctor.core.network.BaseResponse
 import com.umc.hellodoctor.feature.auth.data.model.SocialLoginRequest
 import com.umc.hellodoctor.feature.auth.data.model.SocialLoginResponse
 import com.umc.hellodoctor.feature.auth.data.repository.AuthApi
@@ -10,7 +11,7 @@ class AuthRepositoryImpl @Inject constructor(
     private val authApi: AuthApi
 ) : AuthRepository {
 
-    override suspend fun loginWithSocial(user: SocialUser): SocialLoginResponse {
+    override suspend fun loginWithSocial(user: SocialUser): BaseResponse<SocialLoginResponse> {
         val request = SocialLoginRequest(
             idToken = user.idToken,
         )
---
+++ app/src/main/java/com/umc/hellodoctor/feature/navermap/data/HospitalItem.kt
@@ -0,0 +1,12 @@
+package com.umc.hellodoctor.feature.navermap.data
+
+data class HospitalItem(
+    val id: String,
+    val name: String,
+    val address: String,
+    val tel: String,
+    val distance: Double,
+    val latitude: Double,
+    val longitude: Double,
+    val businessHours: String
+)
