8661a85c52f796e048ad13fd8d68292b5439415e
sfpahs <119095356+sfpahs@users.noreply.github.com>
2026-02-11 15:19:30 +0900

    feat : 결과창 생성 (필드, 지도 선택창)

---
+++ app/src/main/java/com/umc/hellodoctor/feature/chat/presentation/ChatResultPagerAdapter.kt
@@ -0,0 +1,24 @@
+package com.umc.hellodoctor.feature.chat.presentation
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.umc.hellodoctor.feature.navermap.presentation.NaverMapFragment
+
+class ChatResultPagerAdapter(
+    fragmentActivity: FragmentActivity
+) : FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> FieldModeFragment()
+            1 -> {
+                NaverMapFragment()
+            }
+            else -> throw IllegalStateException("Invalid position $position")
+        }
+    }
+}
+
