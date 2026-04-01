44a1deb22df0208c193f64d67a92045be921bcb4
심규석 <sfpahsdev@gmail.com>
2026-04-01 12:00:37 +0900

    style: apply ktlint formatting

--- app/src/main/java/com/umc/hellodoctor/feature/chat/domain/repository/DepartmentRepository.kt
+++ app/src/main/java/com/umc/hellodoctor/feature/chat/domain/repository/DepartmentRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
+
 /**
  * DepartmentRepository - XML에서 진료과 관련 질문과 정보를 관리
  */
